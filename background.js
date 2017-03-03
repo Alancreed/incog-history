760696d4ae1c9ff290ae7ad3816e2f6ef755c97f
Alankrit <Alancreed@users.noreply.github.com>
2017-03-03 16:09:35 -0700

    Retrieval of stored data. Listing of data on popup, with title and onhover address display.

--- background.js
+++ background.js
@@ -2,22 +2,32 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
   //console.log(changeInfo);
   //console.log(tab);
   //console.log(tab.url);
-  if(changeInfo.status === "complete" && tab.url != "chrome://newtab/")
-    addToStorage(tab.url);
+  if(changeInfo.status === "complete" && tab.url != "chrome://newtab/"  && tab.url != tab.title)
+    addToStorage(tab.url, tab.title);
   }
 );
 
 
-var addToStorage = function(url){
-  console.log("URL: " + url);
+var addToStorage = function(url, title){
+  //read data
+  chrome.storage.local.get("urlList", function (item) {
+    var urlList = new Array();
 
-  var urlList = new Array();
+    if("urlList" in item) {
+      urlList = JSON.parse(item.urlList);
+    }
 
-  chrome.storage.local.set({"urlList" : url}, function() {
+    var entry = {};
+    entry.url = url;
+    entry.title = title;
+
+    //append data
+    urlList.unshift( entry );
+    var arrayString = JSON.stringify(urlList);
+
+    //write data
+  chrome.storage.local.set({"urlList" : arrayString}, function() {
     console.log("Data saved : " + (chrome.runtime.lastError == null));
-  })
-
-  chrome.storage.local.get("urlList", function (items) {
-    console.log(items);
-  })
+    });
+  });
 }
--- popup.js
+++ popup.js
@@ -1,16 +1,67 @@
-function codeAddress() {
-  console.log("update");
-  document.getElementById('date').innerHTML = " Somedata " + new Date();
-
+function initPopup() {
   document.getElementById("clearList").addEventListener("click", function() {
-    console.log("button clicked");
     clearList();
+    populatelist();
   })
+
+  linkAddressLabel = document.getElementById("linkAdd");
+
+  setHistoryCount();
+  populatelist();
 }
+
+function setHistoryCount() {
+  chrome.storage.local.get("urlList", function (item) {
+    var urlList = new Array();
+    var countLabel = document.getElementById("count");
+    if ('urlList' in item) {
+      urlList = JSON.parse(item.urlList);
+      countLabel.innerHTML = "Tab History - " + urlList.length;
+    } else {
+      countLabel.innerHTML = "Tab History - " + 0;
+    }
+  });
+}
+
+function populatelist() {
+
+  var htmlList = document.getElementById("uList");
+  if(htmlList != null)
+    htmlList.remove();
+
+  //read data and create a Lists
+  chrome.storage.local.get("urlList", function (item) {
+    var urlList = new Array();
+
+    if("urlList" in item) {
+      urlList = JSON.parse(item.urlList);
+
+      var div = document.getElementById("listContainer");
+
+      var ul = document.createElement("ul");
+      ul.id = "uList";
+      for(var i = 0; i < urlList.length; i++)
+      {
+        var li = document.createElement("li");
+        var a = document.createElement("a");
+        a.addEventListener("mouseover", function(e) {console.log(e);linkAddressLabel.innerHTML = e.target.href});
+        a.addEventListener("mouseout", function() {linkAddressLabel.innerHTML = ""});
+        a.text = urlList[i].title.substring(0,55);
+        a.href = urlList[i].url;
+        li.appendChild(a);
+        ul.appendChild(li);
+      }
+
+      div.appendChild(ul);
+    }
+  });
+}
+
+
 
 function clearList(){
   console.log("Clear list");
   chrome.storage.local.clear();
 }
 
-window.onload = codeAddress;
+window.onload = initPopup;
